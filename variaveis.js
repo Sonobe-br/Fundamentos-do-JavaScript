cb99b35b09326681af1ce51db012d9c7f5ad6f7d
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2021-07-28 13:04:41 -0300

    calculando área quadrada no js e recebendo o resultado no console

--- variaveis.js
+++ variaveis.js
@@ -1,4 +1,4 @@
-//boolean
+/* //boolean
 const usuarioLogado = true;
 const contaPaga = false;
 
@@ -47,8 +47,8 @@ console.log (area)
 let area;
  */
  
-//const - variável imutavel, inauteravel  
-const forma = 'triangulo';
+/* //const - variável imutavel, inauteravel  
+/* const forma = 'triangulo';
 const altura = 5;
 const comprimento = 7;
 let area = null;
@@ -58,6 +58,65 @@ if (forma === 'quadrado') {
     area = altura * comprimento;
 } else {
     area = (altura * comprimento) / 2;
-}
+} */
 
-console.log(area)
+/* console.log(area) */
+
+/* let varInicial = 0;
+let ifFalse = 0;
+let constDeTexto = "Alura"; */
+
+/* var respostaDeTudo = 42
+let idade = 29
+const pi = 3.14
+{
+    var respostaDeTudo = 3.14
+    let idade = 42
+    const pi = 29
+    console.log(respostaDeTudo, idade, pi)
+
+}
+console.log(respostaDeTudo, idade, pi) */
+
+//Hardyfloor
+//laboratório
+let lab1;
+let labArea1Largura = 5.75;
+let labArea1Comprimento = 5.15;
+
+let lab2;
+let labArea2Largura = 10.42;
+let labArea2Comprimento = 4.22;
+
+let area1;
+
+lab1 = labArea1Comprimento * labArea1Largura;
+lab2 = labArea2Comprimento * labArea2Largura;
+
+console.log(lab1)
+console.log(lab2)
+
+area1 = lab1 + lab2
+console.log(area1)
+
+//sala de reunião
+let sr1;
+let salaReuniao1Largura = 7.92;
+let salaReuniao1Comprimento = 4.88;
+
+let sr2;
+let salaReuniao2Largura = 3.54;
+let salaReuniao2Comprimento = 1.55;
+
+let area2;
+
+sr1 = salaReuniao1Comprimento * salaReuniao1Largura;
+sr2 = salaReuniao2Comprimento * salaReuniao2Largura;
+
+console.log(sr1)
+console.log(sr2)
+
+area2 = sr1 + sr2;
+console.log(area2)
+
+console.log(area1 + area2)
