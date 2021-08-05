ec0c125d90f93c1916f96bbb046fe45dbb13aceb
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2021-08-05 11:44:50 -0300

    Funções e Operadores

--- arrow-function.js
+++ arrow-function.js
@@ -1,17 +1,29 @@
-function apresentar(nome) {
+/* function apresentar(nome) {
     return `Meu nome é ${nome}`;
-}
+} */
 //Arrow Function entrou na vida dos Devs a partir da versão ES6 2015
-const apresentarArrow = nome => `Meu nome é ${nome}`;
+/* const apresentarArrow = nome => `Meu nome é ${nome}`;
 console.log(apresentarArrow('Daniel'));
 
 const somar (num1, num2) => num1 + num2;
-
+ */
 //Arrow Function com + de 1 linha de instrução
-const somarNumerosPequenos (num1, num2) => {
+/* const somarNumerosPequenos = (num1, num2) => {
     if (num1 || num2 > 10) {
         return "Somente números de 1 a 9"
     } else {
         return num1 + num2;
     }
 }
+ */
+const num1 = 5;
+const num2 = 10;
+const resultado = num1 + num2;
+
+function soma(num1, num2) {
+    return num1 + num2
+}
+console.log(soma(5, 10)); 
+
+
+
